1f2653f8438aec318c4d4974910e48624c498341
taborgreat <taborgreat@gmail.com>
2025-10-28 05:06:04 -0700

    updated sockets

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,27 +1,39 @@
 import { io } from "socket.io-client";
 import { useEffect } from "react";
 
-export const socket = io("https://tree.tabors.site/", {
+const socket = io("https://tree.tabors.site/", {
   withCredentials: true,
   autoConnect: false,
 });
 
+const registeredPairs = new Set();
+
 export function useSocket(root, username) {
   useEffect(() => {
     if (!root?._id || !username) return;
+
+    const key = `${root._id}:${username}`;
 
     if (!socket.connected) {
       socket.connect();
     }
 
     const onConnect = () => {
-      socket.emit("register", { rootId: root._id, username });
+      if (!registeredPairs.has(key)) {
+        socket.emit("register", { rootId: root._id, username });
+        registeredPairs.add(key);
+        console.log("Registered socket for", key);
+      } else {
+        console.log("Socket already registered for", key);
+      }
     };
 
     socket.on("connect", onConnect);
 
     return () => {
       socket.off("connect", onConnect);
+      registeredPairs.delete(key);
+      console.log(registeredPairs);
     };
   }, [root?._id, username]);
 
